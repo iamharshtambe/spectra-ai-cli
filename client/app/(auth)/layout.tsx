4e9da409e40d45b3d85b0d80dd61d8c2edadf445
Harsh Tambe <harshtambe.dev@gmail.com>
2026-01-03 10:53:09 +0530

    Delete client directory

--- client/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center overflow-hidden">
-      {children}
-    </div>
-  );
-};
-
-export default AuthLayout;
